bcc68d92861a36d6f9a26d1e2736786bff2a54ef
BabacarCoding <babacardrame2007@gmail.com>
2022-06-20 12:34:21 -0400

    Condensed Code

--- code.py
+++ code.py
@@ -5,13 +5,14 @@ print("This random number guesser has three dificulties: Easy, Medium, and Hard.
 difficulty = input("Type your desired difficulty: ")
 difficulty = (difficulty.lower()).strip()
 
-if difficulty == "easy":
+
+def guessFunction(upperBound):
     triesAvailable = 5
-    number = random.randint(1, 10)
+    number = random.randint(1, upperBound)
     guess = 0
     while triesAvailable > 0:
         triesAvailable -= 1
-        guess = int(input("Guess a number between 1 and 10: "))
+        guess = int(input(f"Guess a number between 1 and {upperBound}: "))
         if guess < number:
             print("Your guess is too low")
         elif guess > number:
@@ -23,37 +24,15 @@ if difficulty == "easy":
         print(
             f"You have ran out of guesses. The number was {number}. Better luck next time!")
 
+
+if difficulty == "easy":
+
+    guessFunction(10)
+
 elif difficulty == "medium":
-    triesAvailable = 5
-    number = random.randint(1, 20)
-    guess = 0
-    while triesAvailable > 0:
-        triesAvailable -= 1
-        guess = int(input("Guess a number between 1 and 20: "))
-        if guess < number:
-            print("Your guess is too low")
-        elif guess > number:
-            print("Your guess is too high")
-        elif guess == number:
-            print(f"You guessed the number in {5 - triesAvailable} tries!")
-            triesAvailable = -1
-    if triesAvailable == 0:
-        print(
-            f"You have ran out of guesses. The number was {number}. Better luck next time!")
+
+    guessFunction(20)
+
 elif difficulty == "hard":
-    triesAvailable = 5
-    number = random.randint(1, 30)
-    guess = 0
-    while triesAvailable > 0:
-        triesAvailable -= 1
-        guess = int(input("Guess a number between 1 and 30: "))
-        if guess < number:
-            print("Your guess is too low")
-        elif guess > number:
-            print("Your guess is too high")
-        elif guess == number:
-            print(f"You guessed the number in {5 - triesAvailable} tries!")
-            triesAvailable = -1
-    if triesAvailable == 0:
-        print(
-            f"You have ran out of guesses. The number was {number}. Better luck next time!")
+
+    guessFunction(30)
